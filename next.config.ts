082834491c689065f653a93d7beff84d5d43faab
Fa_ishal21 <faisal2017bahi@gmail.com>
2026-06-16 11:58:37 +0700

    post-v1.2: - update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Jika Development build, gunakan React Server Components untuk meningkatkan performa
+  reactCompiler: true,
+  output: 'standalone',
+  
+  // Jika production build, un-command ini untuk menghasilkan static export
+  // output: 'export',      
+  // images: { unoptimized: true },
 };
 
 export default nextConfig;
